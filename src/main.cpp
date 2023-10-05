5ebea26c2e1691f2a82439511ebd88ffa5b9ea0a
Den <138505681+dnkly@users.noreply.github.com>
2023-10-05 17:10:13 +0300

    add command line interface for encoding mode

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,52 @@
 
 #include <iostream>
 #include <fstream>
+#include <filesystem>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
 
 int main(int argc, char* argv[]) {
-    std::ifstream ifs(argv[1], std::ios::binary);
-    std::ofstream ofs("test.hff", std::ios::binary);
+    po::options_description desc("Options");
+    desc.add_options()
+        ("help,h", "display help message")
+        ("encode,e", "encode data")
+        ("file,f", po::value<std::string>(), "input file")
+        ("output,o", po::value<std::string>(), "output file");
 
-    Huffman h;
-    h.encode(ifs, ofs);
+    po::positional_options_description pos;
+    pos.add("file", -1);
+
+    try {
+        po::variables_map vm;
+        po::store(po::command_line_parser(argc, argv).options(desc).positional(pos).run(), vm);
+        po::notify(vm);
+
+        if (!vm.count("file")) {
+            std::cout << desc;
+            return 0;
+        }
+
+        Huffman h;
+        std::filesystem::path filepath(vm["file"].as<std::string>());
+
+        if (vm.count("encode")) {
+            std::ifstream ifs(vm["file"].as<std::string>(), std::ios::binary);
+
+            if (!ifs.is_open())
+                throw std::system_error(errno, std::generic_category());
+
+            std::string output = vm.count("output")
+                ? vm["output"].as<std::string>()
+                : (filepath.parent_path() / filepath.stem()).string() + ".hff";
+
+            std::ofstream ofs(output, std::ios::binary);
+            h.encode(ifs, ofs);
+        }
+    } catch (std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
